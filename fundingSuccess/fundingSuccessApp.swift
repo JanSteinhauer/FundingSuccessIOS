680c1fcc5c7ff22099f2b2f426234f537463fa3c
Steinhauer <jas27346@glib-a49424.austin.utexas.edu>
2024-08-27 09:28:02 -0500

    firebase added 2

--- fundingSuccess/fundingSuccessApp.swift
+++ fundingSuccess/fundingSuccessApp.swift
@@ -6,14 +6,33 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+
+                   didFinishLaunchingWithOptions launchOptions:
+[UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+    FirebaseApp.configure()
+
+    return true
+
+  }
+
+}
+
 
 @main
 struct fundingSuccessApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandingPageView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
